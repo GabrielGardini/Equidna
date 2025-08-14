9ac75521e3d377becaf75100757113387416e69b
Gabriel Gardini <gabrielgardini1@gmail.com>
2025-08-14 08:24:30 -0300

    Initial Commit

---
+++ Equidna/EquidnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquidnaApp.swift
+//  Equidna
+//
+//  Created by Gabriel Gardini on 14/08/25.
+//
+
+import SwiftUI
+
+@main
+struct EquidnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
