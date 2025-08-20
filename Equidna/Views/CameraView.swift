a20174654b063988507ee9b5392395aa670d854a
Camille Fernandes Luppi <camille@MacBook-Pro-10.local>
2025-08-20 10:09:13 -0300

    primeira alteracao - teste merge

---
+++ Equidna/Views/CameraView.swift
@@ -0,0 +1,7 @@
+//
+//  CameraView.swift
+//  Movies
+//
+//  Created by Camille Fernandes Luppi on 20/08/25.
+//
+
---
+++ Views/CameraView.swift
@@ -0,0 +1,7 @@
+//
+//  CameraView.swift
+//  Equidna
+//
+//  Created by Camille Fernandes Luppi on 20/08/25.
+//
+
