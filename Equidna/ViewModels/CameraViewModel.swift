c218e6baf6d15951bda3594a342a59864d24f943
Camille Fernandes Luppi <camille@10-40-55-60.0200.ecps.br.iosda.org>
2025-08-22 11:40:47 -0300

    xcodegen

--- Equidna/ViewModels/CameraViewModel.swift
+++ Equidna/ViewModels/CameraViewModel.swift
@@ -7,4 +7,57 @@
 
 import SwiftUI
 import UIKit
+import AVKit
+import UniformTypeIdentifiers
 
+struct MediaPicker: UIViewControllerRepresentable {
+
+    @Binding var selectedImage: UIImage?
+    @Binding var videoURL: URL?
+    @Environment(\.presentationMode) private var presentationMode
+
+    var sourceType: UIImagePickerController.SourceType = .camera
+    
+    var videoTimeLimit: TimeInterval = 30.0
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let mediaPicker = UIImagePickerController()
+        mediaPicker.sourceType = sourceType
+        
+        mediaPicker.mediaTypes = [UTType.image.identifier, UTType.movie.identifier]
+
+        mediaPicker.delegate = context.coordinator
+        return mediaPicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: MediaPicker
+
+        init(_ parent: MediaPicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+                parent.videoURL = nil
+            }
+            else if let url = info[.mediaURL] as? URL {
+                parent.videoURL = url
+                parent.selectedImage = nil
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
--- Equidna/Views/CameraView.swift
+++ Equidna/Views/CameraView.swift
@@ -5,3 +5,55 @@
 //  Created by Camille Fernandes Luppi on 20/08/25.
 //
 
+import SwiftUI
+import AVKit
+
+struct CameraView: View {
+    @State private var isShowingMediaPicker = false
+    @State private var inputImage: UIImage?
+    @State private var videoURL: URL?
+
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 30) {
+                
+                if let inputImage = inputImage {
+                    Image(uiImage: inputImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxHeight: 400)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                } else if let videoURL = videoURL {
+                    VideoPlayer(player: AVPlayer(url: videoURL))
+                        .frame(maxHeight: 400)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                } else {
+                    Image(systemName: "photo.on.rectangle.angled")
+                        .font(.system(size: 100))
+                        .foregroundColor(.gray.opacity(0.5))
+                }
+                
+                Button {
+                    self.inputImage = nil
+                    self.videoURL = nil
+                    self.isShowingMediaPicker = true
+                } label: {
+                    HStack {
+                        Image(systemName: "camera.fill")
+                        Text("Abrir Câmera")
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                }
+            }
+            .padding()
+            .navigationTitle("Camera")
+            .fullScreenCover(isPresented: $isShowingMediaPicker) {
+                MediaPicker(selectedImage: $inputImage, videoURL: $videoURL, sourceType: .camera)
+                    .ignoresSafeArea()
+            }
+        }
+    }
+}
